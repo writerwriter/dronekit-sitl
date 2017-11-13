872dde139df845282a6f0b03856981e48c65c1d9
吳承儒 <wuchengru@wuchengrude-MacBook-Pro.local>
2017-11-13 17:38:02 +0800

    renamed test_goto to takeoff and delete conditionyaw.

--- takeoff.py
+++ takeoff.py
@@ -11,25 +11,6 @@ vehicle = connect(args.connect, baud=57600, wait_ready=True)
 #print "Armed: %s" % vehicle.armed
 #print "System status: %s" % vehicle.system_status.state
 
-def condition_yaw(heading, relative=False):
-    if relative:
-        is_relative=1 #yaw relative to direction of travel
-    else:
-        is_relative=0 #yaw is an absolute angle
-    # create the CONDITION_YAW command using command_long_encode()
-    msg = vehicle.message_factory.command_long_encode(
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
-        0, #confirmation
-        heading,    # param 1, yaw in degrees
-        0,          # param 2, yaw speed deg/s
-        1,          # param 3, direction -1 ccw, 1 cw
-        is_relative, # param 4, relative offset 1, absolute angle 0
-        0, 0, 0)    # param 5 ~ 7 not used
-    # send command to vehicle
-    vehicle.send_mavlink(msg)
-    vehicle.flush()
-
 def arm_and_takeoff(aTargetAltitude):
         print "Basic pre-arm checks"
         while not vehicle.is_armable:
@@ -56,10 +37,6 @@ def arm_and_takeoff(aTargetAltitude):
 arm_and_takeoff(2)
 time.sleep(10)
 print "takeoff and hover complete"
-condition_yaw(180)
-print "turn 180 degrees complete"
-time.sleep(20)
-
 print "Mission complete! Time to land~"
 vehicle.mode=VehicleMode("LAND")
 
