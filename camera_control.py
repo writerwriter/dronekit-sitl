c85f253b5df85fc6db36fdea6e9280f9accc0898
Sam <sam19970416@gmail.com>
2018-01-26 17:08:51 +0800

    testing video recording function

--- camera_control.py
+++ camera_control.py
@@ -4,10 +4,16 @@ def pi_camera_capture(camera):
     camera.capture("../picture/"+time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())+".png")
     time.sleep(1)
 def pi_camera_recording(camera):
-	camera.start_recording("../video/"+time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())+".h264")
+	camera.start_recording(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())+".h264")
 def pi_camera_resolution(camera, length, width):
 	camera.resolution = (length, width)
 if __name__ == '__main__':
 	camera = picamera.PiCamera()
-	pi_camera_resolution(camera, 1920, 1080)
-	pi_camera_capture(camera)
+	print "a"
+	pi_camera_resolution(camera,640,480)
+	print "b"
+	camera.start_recording(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())+".h264")
+	print "c"
+	camera.wait_recording(10)
+	camera.stop_recording()
+	print "d"
