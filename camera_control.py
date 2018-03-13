f5585f675d41520638f3f04b870f9eab454201ff
Sam <sam19970416@gmail.com>
2018-03-13 17:02:09 +0800

    Add remote transfer file

--- camera_control.py
+++ camera_control.py
@@ -8,12 +8,19 @@ def pi_camera_recording(camera):
 def pi_camera_resolution(camera, length, width):
 	camera.resolution = (length, width)
 if __name__ == '__main__':
+	
+	choice = raw_input('1.photo 2.recording : ')
 	camera = picamera.PiCamera()
-	print "a"
-	pi_camera_resolution(camera,640,480)
-	print "b"
-	camera.start_recording(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())+".h264")
-	print "c"
-	camera.wait_recording(10)
-	camera.stop_recording()
-	print "d"
+	print "choice is %d" % int(choice)
+	if int(choice) == 1:
+		print "test capture"
+		camera.capture('./cameratest/test_pi.jpg')
+	elif int(chlice) == 2:
+		print "get camera object"
+		pi_camera_resolution(camera,640,480)
+		print "change resolution to 640x480"
+		camera.start_recording(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())+".h264")
+		print "start recording for 10 seconds"
+		camera.wait_recording(10)
+		camera.stop_recording()
+		print "finish recording"
