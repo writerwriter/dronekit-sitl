0c68e7a71e85d71048ee6dd96701dfd2de928425
Sam <sam19970416@gmail.com>
2018-03-13 17:23:28 +0800

    resolve

--- camera_control.py
+++ camera_control.py
@@ -4,18 +4,15 @@ def pi_camera_capture(camera):
     camera.capture("../picture/"+time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())+".png")
     time.sleep(1)
 def pi_camera_recording(camera):
-<<<<<<< HEAD
 	camera.start_recording(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())+".h264")
-=======
-	camera.start_recording("time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())+".h264")
->>>>>>> 6edb4b5da80281f7106a1348ad04e0b0db48fbba
+
 def pi_camera_resolution(camera, length, width):
 	camera.resolution = (length, width)
 if __name__ == '__main__':
 	
 	choice = raw_input('1.photo 2.recording : ')
 	camera = picamera.PiCamera()
-<<<<<<< HEAD
+
 	print "choice is %d" % int(choice)
 	if int(choice) == 1:
 		print "test capture"
@@ -29,8 +26,3 @@ if __name__ == '__main__':
 		camera.wait_recording(10)
 		camera.stop_recording()
 		print "finish recording"
-=======
-	pi_camera_resolution(camera, 1920, 1080)
-	camera.resolution = (1920, 1080)
-	pi_camera_capture(camera)
->>>>>>> 6edb4b5da80281f7106a1348ad04e0b0db48fbba
