c9634b3be4b4dd5e833f01e5c86e34db22010357
System Administrator <root@wuchengrude-MacBook-Pro.local>
2018-04-16 15:06:44 +0800

    upload test file for camera link to sql

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 import MySQLdb
+import sql.py
+import camera_control.py as cc
+import picamera
 
 db = MySQLdb.connect(host="120.126.145.102",user="drone",passwd="dronemysql",db="106project")
 
+camera = picamera.PiCamera() 
+value = cc.capture(camera,"test","test")
+print "locate at %s \n" % value
 missionid = 48
 pointnum = 2
-image_str = "image/test/123"
+image_str = value
 txt_str = "test_string"
 
 cursor = db.cursor()
