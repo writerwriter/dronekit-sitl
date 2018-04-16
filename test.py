e2aef7bd39c94a4abf3483545079173bd2913a76
吳承儒 <wuchengru@wuchengrude-MacBook-Pro.local>
2018-04-16 17:26:07 +0800

    sql new file

--- test.py
+++ test.py
@@ -1,20 +1,23 @@
 import MySQLdb
-import sql.py
-import camera_control.py as cc
-import picamera
+import sql
+#import camera_control as cc
+#import picamera
 
 db = MySQLdb.connect(host="120.126.145.102",user="drone",passwd="dronemysql",db="106project")
 
-camera = picamera.PiCamera() 
-value = cc.capture(camera,"test","test")
-print "locate at %s \n" % value
+#camera = picamera.PiCamera() 
+#value = cc.capture(camera,"test","test")
+#print "locate at %s \n" % value
 missionid = 48
 pointnum = 2
-image_str = value
+image_str = "/test_value"
 txt_str = "test_string"
 
 cursor = db.cursor()
 
-cursor.execute("INSERT iNTO image_display(mission_id, pointNum,image, txt) VALUES (%s,%s,%s,%s)",(missionid,pointnum,image_str,txt_str))
+sql_str = (missionid,pointnum,image_str,txt_str)
+print sql_str
+cursor.execute("INSERT iNTO image_display(mission_id, pointNum,image, txt) VALUES (%s,%s,%s,%s)",sql_str)
+#cursor.execute(sql_str)
 cursor.close()
 db.commit()
