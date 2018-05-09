780fdbc896291126ebb69615615a66d256af138f
System Administrator <root@wuchengrude-MacBook-Pro.local>
2018-05-09 18:48:08 +0800

    fsafdsafs

--- velocity_control_5m.py
+++ velocity_control_5m.py
@@ -43,45 +43,45 @@ msg_Anchor1_inverse = Drone.return_send_ned_velocity_mavlink_msg(vehicle,0.15,-0
 msg_Anchor2 = Drone.return_send_ned_velocity_mavlink_msg(vehicle,-0.15,-0.26,0)
 msg_Anchor2_inverse = Drone.return_send_ned_velocity_mavlink_msg(vehicle,0.15,0.26,0)
 			
-	while True:
-		offset = 30
-		report = Get_uwb_distance()
-		print report[0]
-		print report[1]
-		print report[2]
-		if abs(report[0]-report[1]) < offset and abs(report[0]-report[2]) < offset and abs(report[1]-report[2]) < offset:
-			break
-		elif report[0] > report[1] and report[0] > report[2]:
-			if abs(report[0]-report[1]) < offset:
-				print "go to anchor 2 inverse"
-				vehicle.send_mavlink(msg_Anchor2_inverse)
-			elif abs(report[0]-report[2]) < offset:
-				print "go to anchor 1 inverse"
-				vehicle.send_mavlink(msg_Anchor1_inverse)
-			elif:
-				print "go to anchor 0"
-				vehicle.send_mavlink(msg_Anchor0)
-		elif report[1] > report[0] and report[1] > report[2]:
-			if abs(report[1]-report[0]) < offset:
-				print "go to anchor 2 inverse"
-				vehicle.send_mavlink(msg_Anchor2_inverse)
-			elif abs(report[1]-report[2]) < offset:
-				print "go to anchor 0 inverse"
-				vehicle.send_mavlink(msg_Anchor0_inverse)
-			elif:
-				print "go to anchor 1"
-				vehicle.send_mavlink(msg_Anchor1)
-		elif report[2] > report[0] and report[2] > report[1]:
-			if abs(report[2]-report[0]) < offset:
-				print "go to anchor 1 inverse"
-				vehicle.send_mavlink(msg_Anchor1_inverse)
-			elif abs(report[2]-report[1]) < offset:
-				print "go to anchor 0 inverse"
-				vehicle.send_mavlink(msg_Anchor0_inverse)
-			elif:
-				print "go to anchor 2"
-				vehicle.send_mavlink(msg_Anchor2)
-		time.sleep(1)
+while True:
+	offset = 30
+	report = Get_uwb_distance()
+	print report[0]
+	print report[1]
+	print report[2]
+	if abs(report[0]-report[1]) < offset and abs(report[0]-report[2]) < offset and abs(report[1]-report[2]) < offset:
+		break
+	elif report[0] > report[1] and report[0] > report[2]:
+		if abs(report[0]-report[1]) < offset:
+			print "go to anchor 2 inverse"
+			vehicle.send_mavlink(msg_Anchor2_inverse)
+		elif abs(report[0]-report[2]) < offset:
+			print "go to anchor 1 inverse"
+			vehicle.send_mavlink(msg_Anchor1_inverse)
+		elif:
+			print "go to anchor 0"
+			vehicle.send_mavlink(msg_Anchor0)
+	elif report[1] > report[0] and report[1] > report[2]:
+		if abs(report[1]-report[0]) < offset:
+			print "go to anchor 2 inverse"
+			vehicle.send_mavlink(msg_Anchor2_inverse)
+		elif abs(report[1]-report[2]) < offset:
+			print "go to anchor 0 inverse"
+			vehicle.send_mavlink(msg_Anchor0_inverse)
+		elif:
+			print "go to anchor 1"
+			vehicle.send_mavlink(msg_Anchor1)
+	elif report[2] > report[0] and report[2] > report[1]:
+		if abs(report[2]-report[0]) < offset:
+			print "go to anchor 1 inverse"
+			vehicle.send_mavlink(msg_Anchor1_inverse)
+		elif abs(report[2]-report[1]) < offset:
+			print "go to anchor 0 inverse"
+			vehicle.send_mavlink(msg_Anchor0_inverse)
+		elif:
+			print "go to anchor 2"
+			vehicle.send_mavlink(msg_Anchor2)
+	time.sleep(1)
 
 
 vehicle.mode = VehicleMode("LAND")
